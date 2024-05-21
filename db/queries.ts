d2003e1833030e0f257b1601f56a3f15cd3254cc
TManhique <manhiquet0@gmail.com>
2024-05-21 19:36:52 +0200

    feat: Add functionality to resume user progress
    - Implemented feature to save and retrieve user progress
    - Users can now continue from where they left off
    - Updated UI to reflect resume options
    - Added necessary backend support to handle progress data

--- db/queries.ts
+++ db/queries.ts
@@ -1,6 +1,25 @@
 import { cache } from "react";
+import { eq } from "drizzle-orm";
 import db from "./drizzle";
+import { auth } from "@clerk/nextjs";
+import { userProgress } from "./schema";
 
+export const getUserProgress = cache(async () => {
+    const { userId } = await auth();
+
+    if (!userId){
+        return null;
+    }
+
+    const data = db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true,
+        },
+    });
+    return data;
+
+})
 
 export const  getCourses = cache(async ()  => {
     const data = await db.query.courses.findMany();
@@ -8,3 +27,4 @@ export const  getCourses = cache(async ()  => {
     return data;
 });
  
+
