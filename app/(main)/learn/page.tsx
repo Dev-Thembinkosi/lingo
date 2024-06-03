492a7b018232a17a136ef5483a596b42c1251cf6
TManhique <manhiquet0@gmail.com>
2024-06-03 12:22:56 +0200

    Add unit banner component

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,13 +1,17 @@
 import { redirect } from "next/navigation";
 import { StickyWrapper } from "@/components/sticky-wrapper";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import { FeedWrapper } from "@/components/feed-wrapper";
-import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+
+import { Header } from "./header";
+import { Unit } from "./unit";
 
 const LearnPage = async () => {
     const userProgressData = getUserProgress();
-    const [userProgress] = await Promise.all([userProgressData]);
+    const unitsData = getUnits()
+
+    const [userProgress, units] = await Promise.all([userProgressData, unitsData]);
 
     if (!userProgress || !userProgress.activeCourse) {
         redirect("/courses")
@@ -26,6 +30,19 @@ const LearnPage = async () => {
             </StickyWrapper>
             <FeedWrapper>
                 <Header title={userProgress.activeCourse.title}/>
+                {units.map((unit) => (
+                    <div key={unit.id} className="mb-10">
+                        <Unit
+                            id={unit.id}
+                            order={unit.order}
+                            description={unit.description} 
+                            title={unit.title}
+                            lessons={unit.lessons}
+                            activeLesson={undefined}
+                            activeLessonPercentage={0}
+                        />
+                    </div>
+                ))}
          
             </FeedWrapper>
         </div>
--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import { eq } from "drizzle-orm";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs";
-import { courses, units, userProgress } from "./schema";
+import { challengeProgress, courses, units, userProgress } from "./schema";
 
 
 
@@ -24,12 +24,15 @@ export const getUserProgress = cache(async () => {
 
 });
 
-export const getUnit = cache(async () =>{
+export const getUnits = cache(async () => {
+
+    const { userId } = await auth();
     const userProgress = await getUserProgress();
 
-    if(!userProgress?.activeCourseId){
+    if(!userId || !userProgress?.activeCourseId){
         return [];
     }
+
 
     const data = await db.query.units.findMany({
         where: eq(units.courseId, userProgress.activeCourseId),
@@ -38,7 +41,12 @@ export const getUnit = cache(async () =>{
                 with: {
                     challenges: {
                         with:{
-                            challengeProgress: true,
+                            challengeProgress: {
+                            where: eq(
+                                challengeProgress.userId,
+                                userId,
+                            ),
+                        }
                         },
                     },
                 },
