906f7969bd2bf78b4cdd559af313d24513e18465
TManhique <manhiquet0@gmail.com>
2024-06-19 05:51:56 +0200

    Lesson header component update, progress and hearts addition

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,3 +1,7 @@
+import { Progress } from "@/components/ui/progress";
+import { InfinityIcon, X } from "lucide-react"; 
+import Image from "next/image";
+
 type Props = {
     hearts: number;
     percentage :number;
@@ -13,8 +17,27 @@ export const Header = ({
     hasActiveSubscription,
 }: Props) => {
     return (
-        <div>
-            Header
-        </div>
+        <header className="gl:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
+            <X 
+                onClick={() =>{}} //TODO: Add a onCLick exit
+                className="text-slate-500 hover:opacity-75 transition cursor-pointer"
+            
+            
+            />
+
+            <Progress value={percentage}/>
+            <div className="text-rose-500 flex items-center font-bold">
+                <Image 
+                    src="heart.svg"
+                    height={28}
+                    width={28}
+                    alt="Heart"
+                    className="mr-2"
+                />
+
+                {hasActiveSubscription? <InfinityIcon className="h-6 w-6 stroke-{3}"/> : hearts}
+
+            </div>
+        </header>
     )
 }
