f6385e30f8bef03b4859c6d9f749bb6ec4022b48
TManhique <manhiquet0@gmail.com>
2024-06-19 06:02:52 +0200

    Update: Lesson header progressbar indicator color update

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -19,7 +19,7 @@ export const Header = ({
     return (
         <header className="gl:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
             <X 
-                onClick={() =>{}} //TODO: Add a onCLick exit
+                onClick={() =>{}}
                 className="text-slate-500 hover:opacity-75 transition cursor-pointer"
             
             
