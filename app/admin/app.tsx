d1da8462aede2cddd20ce8b80b67859a81bc8428
TManhique <manhiquet0@gmail.com>
2024-07-13 11:48:18 +0200

    amdin lesson edit and crate components

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -2,10 +2,18 @@
 
 import { Admin, Resource} from "react-admin";
 import simpleRestProvider from "ra-data-simple-rest";
+
 import { CourseList } from "./course/list";
 import { CourseCreate } from "./course/create";
 import { CourseEdit } from "./course/edit";
+
 import { UnitList } from "./unit/list";
+import { UnitCreate } from "./unit/create";
+import { UnitEdit } from "./unit/edit";
+
+import { LessonEdit } from "./lesson/edit";
+import { LessonCreate } from "./lesson/create";
+import { LessonList } from "./lesson/list";
 
 const dataProvider = simpleRestProvider("/api")
 
@@ -24,8 +32,16 @@ const App = () => {
         <Resource 
             name="units" 
             list={UnitList} 
-            create={CourseCreate}
-            edit={CourseEdit}
+            create={UnitCreate}
+            edit={UnitEdit}
+            recordRepresentation="title" 
+        />
+
+        <Resource 
+            name="lessons" 
+            list={LessonList} 
+            create={LessonCreate}
+            edit={LessonEdit}
             recordRepresentation="title" 
         />
     </Admin>
